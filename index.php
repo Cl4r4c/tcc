ebc8e0945e404baa8df30dd38da0c246a9ef5602
Clara Chaves <161716144+Cl4r4c@users.noreply.github.com>
2024-05-29 20:16:59 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,113 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>healer__tec</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">  
+  </head>
+  <body 
+  background-color: #0D47A1;>
+  <nav class="navbar bg-primary" data-bs-theme="dark">
+  <div class="container-fluid">
+  <nav class="navbar bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="HEALERFUNDOBRANCO-bg.png.png" alt="Logo" width="100" height="20" class="d-inline-block align-text-top">
+    </a>
+  </div>
+</nav>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="empresa.php">Sobre nós</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="servicos.php">Serviços</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="contato.php" >Contato</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<br>
+<div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="1.png" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="2.png" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+  <br>
+</div>
+<div class="card text-bg-dark">
+  <img src="bannertxt.png" class="card-img" alt="..." >
+  <div class="card-img-overlay">
+</div>
+<br>
+ </div>
+ <div class="row row-cols-1 row-cols-md-2 g-4">
+  <div class="col">
+    <div class="card text-bg-primary mb-3">
+      <img src="sites.png" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Informatização de empresas</h5>
+        <p class="card-text">Criação de sites, sistemas e informatização de empresas.</p>
+        <button type="button" class="btn btn-outline-light" href="#" >Veja mais</button>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card text-bg-primary mb-3">
+      <img src="manutencao.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Manutenção de computadores</h5>
+        <p class="card-text">Manutenções completas, limpeza de computadores, backup's, atualizações e muito mais.</p>
+        <button type="button" class="btn btn-outline-light" href="#" >Veja mais</button>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card text-bg-primary mb-3">
+      <img src="design.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Design gráfico</h5>
+        <p class="card-text">Suporte em design, reformulção e criação de identidade visual.</p>
+        <button type="button" class="btn btn-outline-light" href="#" >Veja mais</button>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card text-bg-primary mb-3">
+      <img src="cliente.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Excelência em serviços</h5>
+        <p class="card-text">Excelência em execução, atendimento e qualidade. Sempre deixando clientes os satisfeitos.</p>
+        <button type="button" class="btn btn-outline-light" href="#" >Veja mais</button>
+      </div>
+    </div>
+  </div>
+  </div>
+  <div class="card text-center">
+  <div class="card-body">
+    <p class="card-text">Você também pode nos achar em <button type="button" class="btn btn-link"><a href="https://www.instagram.com/healertec?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==">Instagram</a></button> e <button type="button" class="btn btn-link"><a href="https://wa.me/5521999563793?text=Ol%C3%A1,%20vim%20atrav%C3%A9s%20do%20site.%20">WhatsApp</a></button></p>
+  </div>
+</div>
+</div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
